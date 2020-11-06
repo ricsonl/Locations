e80c388b416913977ba9e7faa34f48c8786f0b99
Ricson Luiz <ricsonluizv@gmail.com>
2020-11-06 13:21:01 -0300

    maps api config

--- app/src/main/java/android/teste/locations/MainActivity.java
+++ app/src/main/java/android/teste/locations/MainActivity.java
@@ -1,14 +1,13 @@
 package android.teste.locations;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ListActivity;
-import android.graphics.Color;
+
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends ListActivity {
 
@@ -18,5 +17,20 @@ public class MainActivity extends ListActivity {
         String menu [] = new String [] {"Minha casa na cidade natal", "Minha casa em Viçosa", "Meu departamento", "Fechar aplicação"};
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu);
         setListAdapter(arrayAdapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id){
+        if(position != l.getAdapter().getCount()-1){//Caso não seja a última opção, "Fechar aplicação"
+            String aux = l.getItemAtPosition(position).toString();
+
+            Toast t = Toast.makeText(getBaseContext(), aux,Toast.LENGTH_SHORT);
+            TextView tv = (TextView) t.getView().findViewById(android.R.id.message);
+            tv.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+            tv.setTextColor(getResources().getColor(R.color.colorBackground));
+            t.show();
+        } else {
+            finish();
+        }
     }
 }
