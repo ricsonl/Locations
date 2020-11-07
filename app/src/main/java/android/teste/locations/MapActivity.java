69cac3eff230439e65c4c5c14eaa41173345f1fa
Ricson Luiz <ricsonluizv@gmail.com>
2020-11-06 23:50:09 -0300

    permissions fix, current location etc

--- app/src/main/java/android/teste/locations/MapActivity.java
+++ app/src/main/java/android/teste/locations/MapActivity.java
@@ -1,15 +1,16 @@
 package android.teste.locations;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -25,6 +26,8 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.text.DecimalFormat;
+
 public class MapActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     private GoogleMap map;
@@ -37,21 +40,26 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
     private final LatLng VICOSA = new LatLng(-20.754070, -42.879845);
     private final LatLng DPI = new LatLng(-20.764801, -42.868462);
 
+    public Location vicosa = new Location(Context.LOCATION_SERVICE); // usado para calcular a distancia da posicao atual até o endereço de viçosa
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map_activity);
 
         ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
+
+        vicosa.setLatitude(VICOSA.latitude);
+        vicosa.setLongitude(VICOSA.longitude);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        ActivityCompat.requestPermissions(this,new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         map  = googleMap;
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         map.setBuildingsEnabled(true);
-        requestLocationPermission();
+
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
         map.addMarker(new MarkerOptions().position(ITAGUARA).title("Minha casa em Itaguara"));
         map.addMarker(new MarkerOptions().position(VICOSA).title("Minha casa em Viçosa"));
@@ -73,53 +81,108 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback,
         TextView tv = (TextView) t.getView().findViewById(android.R.id.message);
         tv.setBackgroundColor(getResources().getColor(android.R.color.transparent));
         tv.setTextColor(getResources().getColor(R.color.colorBackground));
+        t.setGravity(Gravity.BOTTOM,0,220);
         t.show();
+
+        requestLocationPermissionIfNeeded();
     }
 
     public void onItaClick(View v){
+        if (usr != null) {// remove o marcador da posicao atual se existir
+            usr.remove();
+        }
         map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         CameraUpdate upd = CameraUpdateFactory.newLatLngZoom(ITAGUARA, 18);
         map.animateCamera(upd);
     }
 
     public void onVicClick(View v){
+        if (usr != null) {// remove o marcador da posicao atual se existir
+            usr.remove();
+        }
         map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
         CameraUpdate upd = CameraUpdateFactory.newLatLngZoom(VICOSA, 18);
         map.animateCamera(upd);
     }
 
     public void onDpiClick(View v){
+        if (usr != null) {// remove o marcador da posicao atual se existir
+            usr.remove();
+        }
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         CameraUpdate upd = CameraUpdateFactory.newLatLngZoom(DPI, 17);
         map.animateCamera(upd);
     }
 
     public void onLocClick(View v){
-        Location l = lm.getLastKnownLocation(lm.GPS_PROVIDER);
-        LatLng newLoc = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        if(checkPermission()){
+            LatLng newLoc = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
 
-        if(usr != null) {
-            usr.remove();
+            if (usr != null) {
+                usr.remove();
+            }
+
+            usr = map.addMarker(new MarkerOptions().position(newLoc)
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                    .title("Localização atual"));
+
+            map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            CameraUpdate upd = CameraUpdateFactory.newLatLngZoom(newLoc, 19);
+            map.animateCamera(upd);
+
+            DecimalFormat df = new DecimalFormat("0.##");
+            Toast t = Toast.makeText(getBaseContext(), "Você está a " + df.format(currentLocation.distanceTo(vicosa)) + " m do endereço em Viçosa",Toast.LENGTH_LONG);
+            TextView tv = (TextView) t.getView().findViewById(android.R.id.message);
+            tv.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+            tv.setTextColor(getResources().getColor(R.color.colorBackground));
+            t.setGravity(Gravity.BOTTOM,0,220);
+            t.show();
+
+        } else {
+            Toast t = Toast.makeText(getBaseContext(), "Você precisa ir às configurações do android e permitir que o aplicativo acesse sua localização!",Toast.LENGTH_LONG);
+            TextView tv = (TextView) t.getView().findViewById(android.R.id.message);
+            tv.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+            tv.setTextColor(getResources().getColor(R.color.colorBackground));
+            t.setGravity(Gravity.BOTTOM,0,220);
+            t.show();
         }
-
-        usr = map.addMarker(new MarkerOptions().position(newLoc)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
-                .title("Localização atual"));
     }
 
-    public void requestLocationPermission(){
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                ||ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+    public void requestLocationPermissionIfNeeded(){
+        if(!checkPermission()){ // verifica se as permissoes ainda nao foram dadas
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);// pede as permissoes e chama onRequestPermissionsResult como callback
         } else {
-            map.setMyLocationEnabled(true);
+            //map.setMyLocationEnabled(true);
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, (LocationListener) this);// configura para atualizar a localização do usuário a cada 5 segundos
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults){
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) { // se o usuario deu as permissoes
+                    if (checkPermission()) {
+                        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, (LocationListener) this);// configura para atualizar a localização do usuário a cada 5 segundos;
+                    }
+                } else {
+                    //
+                }
+            }
+        }
+    }
+
+    public boolean checkPermission() {
+        int FirstPermissionResult = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION);
+        int SecondPermissionResult = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        return FirstPermissionResult == PackageManager.PERMISSION_GRANTED &&
+                SecondPermissionResult == PackageManager.PERMISSION_GRANTED;
     }
 
     @Override
     public void onLocationChanged(Location location) {
         currentLocation = location;
-        System.out.println(location.getLatitude());
     }
 
     @Override
